664861a6411e87c34ab9b9f49b9d745da584885d
Akshit Yadav <akshit07032001@gmail.com>
2025-09-21 15:44:42 +0530

    initial backend setup

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log('*****DB Connected*****');
+  } catch (error) {
+    console.error('*****DB Connection error*****', error);
+  }
+};
+
+export default connectDb;
